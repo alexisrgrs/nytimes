7be3a6e725e8b6baa30fd06014af5a41c10a0302
Brandon <hall.brandon@hotmail.com>
2018-10-27 13:21:50 -0400

    changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,15 +7,45 @@ var $search = $("#search");
 var $clear = $("#clear");
 var $articles = $("#articles");
 
+var apikey = "244ce6d25dc544d2b9ef69153389dd56";
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
 // get the value of search, records, start, and end fields
 $search.on("click", function() {
-    $term.val()
-    $records.val()
-    $start.val()
-    $end.val()
+    var term = $term.val();
+    var records = $records.val();
+    var start = $start.val();
+    var end = $end.val();
 
     console.log($term.val());
-})
+    //console.log("search");
+    var newUrl = url + "?q=" + term;
+    if (start !== "") {
+        newUrl = newUrl + "&begin_date=" + start;
+    }
+
+    if (end !== "") {
+        newUrl = newUrl + "&end_date=" + end;
+    }
+    
+    newUrl = newUrl + "&api-key=" + apikey;
+    
+    $.ajax({
+        url: newUrl,
+        method: "GET"
+    })
+        .then(function(response) {
+        console.log(response.response.docs)
+        var $snippet = $("<h1>").text(response.response.docs[0].snippet);
+
+        var $articleUrl = $("<p>").text(response.response.docs[0].web_url);
+        $articles.append($snippet, $articleUrl);
+
+        
+
+    })
+
+});
 
 
 
